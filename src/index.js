2a3220d1fd954847426d960cf7e5d5b76bcd5732
yasuaki640 <yasuaki640@gmail.com>
2020-01-26 22:24:23 +0900

    change-color.jsを読み込ませる

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Bootstrapのスタイルシート側の機能を読み込む
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// BootstrapのJavaScript側の機能を読み込む
+import "bootstrap";
+
+//文字の色を変えるjsファイル
+import {changeColor} from "./modules/change-color";
+
+changeColor();
