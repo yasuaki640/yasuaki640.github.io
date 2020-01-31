d0ec80cc7836c55067be61f7b13b888a5b95b04e
yasuaki640 <yasuaki640@gmail.com>
2020-01-31 22:44:55 +0900

    iframe用のcssを読み込ませる

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // BootstrapのJavaScript側の機能を読み込む
 import "bootstrap";
 
+//iframeレスポンシブル対応用のCSS
+import "./modules/iframe.css"
+
 //文字の色を変えるjsファイル
 import {setRandomColor} from "./modules/set-random-color";
 
