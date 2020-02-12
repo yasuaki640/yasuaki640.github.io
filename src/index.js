3a9daf232248f35bfc8c4ab617482bb27d87a404
yasuaki640 <yasuaki640@gmail.com>
2020-02-12 22:48:04 +0900

    glotを読み込ませる

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // BootstrapのJavaScript側の機能を読み込む
 import "bootstrap";
 
+// 多言語対応ライブラリを読み込む
+import "glottologist";
+
+const glot = new Glottologist();
+
 //文字の色を変えるjsファイル
 import {setRandomColor} from "./modules/set-random-color";
 
