b28d9f4c0e73e2697efee934d8f9c984adf4d310
yasuaki640 <yasuaki640@gmail.com>
2020-02-13 11:53:25 +0900

    処理をまとめる

--- src/modules/set-random-color.js
+++ src/modules/set-random-color.js
@@ -1,8 +1,8 @@
-export function setRandomColor() {
+function setRandomColor() {
     let rValue = Math.floor(Math.random() * 255);
     let gValue = Math.floor(Math.random() * 255);
     let bValue = Math.floor(Math.random() * 255);
     document.getElementById("changingColor").style.color = `rgb(${rValue},${gValue},${bValue})`;
 }
 
-setInterval(setRandomColor, 100);
+export const changeColor = setInterval(setRandomColor, 100);
