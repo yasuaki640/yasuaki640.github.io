5b02ea1350b69079d2231484c85c47ea5cb495c2
yasuaki640 <yasuaki640@gmail.com>
2020-02-13 11:55:20 +0900

    Update set-random-color.js

--- src/modules/set-random-color.js
+++ src/modules/set-random-color.js
@@ -1,8 +1,10 @@
-function setRandomColor() {
-    let rValue = Math.floor(Math.random() * 255);
-    let gValue = Math.floor(Math.random() * 255);
-    let bValue = Math.floor(Math.random() * 255);
-    document.getElementById("changingColor").style.color = `rgb(${rValue},${gValue},${bValue})`;
-}
+export function changeColor() {
+    function setRandomColor() {
+        let rValue = Math.floor(Math.random() * 255);
+        let gValue = Math.floor(Math.random() * 255);
+        let bValue = Math.floor(Math.random() * 255);
+        document.getElementById("changingColor").style.color = `rgb(${rValue},${gValue},${bValue})`;
+    }
 
-export const changeColor = setInterval(setRandomColor, 100);
+    setInterval(setRandomColor, 100);
+}
