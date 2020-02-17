8edabda619636a55b73def90e3a5f142c12a3dcc
yasuaki640 <yasuaki640@gmail.com>
2020-02-17 21:54:01 +0900

    ディレクトリ構成を変更

---
+++ src/js/set-random-color.js
@@ -0,0 +1,10 @@
+export function changeColor() {
+    function setRandomColor() {
+        let rValue = Math.floor(Math.random() * 255);
+        let gValue = Math.floor(Math.random() * 255);
+        let bValue = Math.floor(Math.random() * 255);
+        document.getElementById("changingColor").style.color = `rgb(${rValue},${gValue},${bValue})`;
+    }
+
+    setInterval(setRandomColor, 100);
+}
