d88773d090ce7fa98be5f474bcfb052b6a775e50
yasuaki640 <yasuaki640@gmail.com>
2020-07-30 20:14:57 +0900

    transport header from old html files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {Nav} from 'react-bootstrap'
+import {Button} from "react-bootstrap";
 
 class Header extends React.Component {
     render() {
         return (
-            <div>this is a header we creating</div>
+            <Nav className="sticky-top navbar-expand-md navbar-light bg-light mb-4">
+                <Button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navber"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="ナビゲーションの切替">
+                    <span className="navbar-toggler-icon"/>
+                </Button>
+                <div className="collapse navbar-collapse" id="navber">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Profile">Profile</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Qualifications">Qualifications</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#MyApps">My Apps</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#Videos">Videos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./index.html" id="ja">日本語</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./index_en.html" id="en">English</a>
+                        </li>
+                    </ul>
+                </div>
+            </Nav>
         );
     }
 }
