4335af1e508fc9b77cefdf7e919594de0f1dbe2d
yasuaki640 <yasuaki640@gmail.com>
2020-07-30 20:13:41 +0900

    import portraits

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import Header from './Header'
 import Footer from "./Footer";
 import Profile from "./Profile";
 import MyaApp from "./MyApp";
+import Portrait from "./Portrait";
 
 function App() {
     return (
         <div id="main-container">
             <Header/>
+            <Portrait/>
             <Profile/>
             <MyaApp/>
             <Footer/>
