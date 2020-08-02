678f5df95aa39dcf94297c25d7d44ef6545d6362
yasuaki640 <yasuaki640@gmail.com>
2020-08-02 09:08:10 +0900

    create a content of qualifications

---
+++ src/components/Qualifications.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Qualifications extends React.Component {
+    render() {
+        return (
+            <div className="content mb-5">
+                <h3 className="mb-3" id="Qualifications">Qualifications</h3>
+                <p>基本情報技術者試験 2019/10</p>
+                <p>Oracle Java Silver 2019/11</p>
+                <p>TOEIC 815 2017/10</p>
+                <p>英検準一級 2018/10</p>
+            </div>
+        );
+    }
+}
+
+export default Qualifications;
