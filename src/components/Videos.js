89222d7018424fe442db15de9f7f87ad07bd0e73
yasuaki640 <yasuaki640@gmail.com>
2020-08-02 14:42:32 +0900

    Revert "separate iframe videos"
    This reverts commit deee0d283db7e52a35dd475e1ee4c96de87a567c.

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import {Row} from "react-bootstrap";
 import {Col} from "react-bootstrap";
-import IframeVideo from "./iframeVideo";
 
 class Videos extends React.Component {
-
     render() {
-        const contentTitle = 'Videos (Playing drums)';
         return (
             <div className="content mb-5">
-                <h3 className="mb-3" id={contentTitle}>{contentTitle}</h3>
-                {this.sentence()}
+                <h3 className="mb-3" id="Videos">Videos (playing drums)</h3>
+                <Row>
+                    <Col md={6}>
+                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/IvJm2414GjY"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen/>
+                    </Col>
+                    <Col md={6}>
+                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/pqDafN7U8B0"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen/>
+                    </Col>
+                </Row>
             </div>
         );
     }
-
-    sentence() {
-        return (
-            <>
-                <Row>
-                    <Col md={6}>
-                        <IframeVideo src="https://www.youtube.com/embed/IvJm2414GjY"/>
-                    </Col>
-                    <Col md={6}>
-                        <IframeVideo src="https://www.youtube.com/embed/CMc1b4Ss2Fs"/>
-                    </Col>
-                </Row>
-            </>
-        );
-    }
-
-
 }
 
 export default Videos;
