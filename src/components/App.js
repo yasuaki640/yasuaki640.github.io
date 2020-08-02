5f80ac025f796d1a030518221373e6f2ce970a47
yasuaki640 <yasuaki640@gmail.com>
2020-08-02 09:09:49 +0900

    import created contents

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header'
-import Footer from "./Footer";
 import Profile from "./Profile";
 import MyaApp from "./MyApp";
 import Portrait from "./Portrait";
+import Videos from "./Videos";
+import Qualifications from "./Qualifications";
+import Title from "./Title";
 
 function App() {
     return (
         <div id="main-container">
             <Header/>
+            <Title/>
             <Portrait/>
             <Profile/>
             <MyaApp/>
-            <Footer/>
+            <Qualifications/>
+            <Videos/>
         </div>
     );
 }
