7b1dba95679f7b6df59e83abf888bdba93ff44f8
yasuaki640 <yasuaki640@gmail.com>
2020-08-05 07:02:41 +0900

    Updates

---
+++ precache-manifest.804ea7e967262876d9b8769ac7ee0366.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d855a1e141ec80acf92109c9ed6b41c",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/index.html"
+  },
+  {
+    "revision": "c4dbeb42ed3719be82a8",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ceb3447af1551393265e",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/css/main.6fe5a8fa.chunk.css"
+  },
+  {
+    "revision": "c4dbeb42ed3719be82a8",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/js/2.64c148b9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/js/2.64c148b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb3447af1551393265e",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/js/main.54633504.chunk.js"
+  },
+  {
+    "revision": "f19f8941b38f4f909bda",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/js/runtime-main.bc93d1fc.js"
+  },
+  {
+    "revision": "ed058b01c61a93ae829ccecffb4d0932",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/media/background_1.ed058b01.jpg"
+  },
+  {
+    "revision": "8dd7c780a373de48e940346dbb49899a",
+    "url": "/yasuaki640.github.io/yasuaki640.github.io/static/media/myPortlait.8dd7c780.jpg"
+  }
+]);
