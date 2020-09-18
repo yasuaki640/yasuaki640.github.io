12e35a6dc016222d1faa54f2119eb902e12be795
yasuaki640 <yasuaki640@gmail.com>
2020-09-18 22:01:23 +0900

    COMPOSITE

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class Content extends React.Component {
 
-    render() {
+    contentContainer(props) {
         const contentTitle = this.props.contentTitle;
         return (
             <div className="content mb-5">
                 <h3 className="mb-3" id={contentTitle}>{contentTitle}</h3>
-                {this.sentence()}
+                {props.children}
             </div>
         );
     }
