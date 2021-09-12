80001b500f4374a4d9b89579c63d0161b51e7405
yasuaki640 <yasuaki640@gmail.com>
2021-09-12 20:38:54 +0900

    ホントはfor文でリファクタしたほうが良いのは知ってます

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -5,27 +5,52 @@ class MyaApp extends React.Component {
     sentence() {
         return (
             <>
-                <p><a href="https://github.com/yasuaki640/yasuaki640.github.io" target="_blank"
-                      rel="noopener noreferrer">A portfolio site integrated by simple raact（this site）</a>
+                <p>
+                    <a href="https://github.com/yasuaki640/yasuaki640.github.io"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                    >
+                        A portfolio site integrated by simple React (this site)
+                    </a>
                 </p>
-                <p><a href="https://github.com/yasuaki640/news-api-app" target="_blank"
-                      rel="noopener noreferrer">A minimal news search app integrated by React and Node.js</a>
+                <p>
+                    <a href="https://github.com/yasuaki640/news-api-app"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                    >
+                        A minimal news search app integrated by React and Node.js
+                    </a>
                 </p>
-                <p><a href="https://github.com/yasuaki640/scraping_images" target="_blank"
-                      rel="noopener noreferrer">A tool for saving images automatically from Instagram.<br/>
-                    (Please use at your own risk because it violates the rules.)</a>
+                <p>
+                    <a href="https://github.com/yasuaki640/scraping_images"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                    >
+                        A tool for saving images automatically from Instagram.<br/>
+                        (Please use at your own risk because it violates the rules.)
+                    </a>
                 </p>
-                <p>An number of people who attend events counting app for religious corporations<br/>
-                    (Private for religious reasons.)</p>
+                <p>
+                    An number of people who attend events counting app for religious corporations<br/>
+                    (Private for religious reasons.)
+                </p>
                 <p>
                     <a href="https://yasuaki640.github.io/Terlis-by-pureJS/"
                        target="_blank"
-                       rel="noopener noreferrer">An tetris game integrated by vanilla JavaScript
-                        <br/>(Not compatible with smartphones)</a>
+                       rel="noopener noreferrer"
+                    >
+                        An tetris game integrated by vanilla JavaScript
+                        <br/>(Not compatible with smartphones)
+                    </a>
                 </p>
                 <p>
-                    <a href="https://sns2-2020.s3-ap-northeast-1.amazonaws.com/login.html" target="_blank"
-                       rel="noopener noreferrer">Bulletin board with login function integrated by Vue.js, Node.js, and AWS Lambda.</a>
+                    <a href="https://sns2-2020.s3-ap-northeast-1.amazonaws.com/login.html"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                    >
+                        Bulletin board with login function integrated by Vue.js, Node.js, and
+                        AWS Lambda.
+                    </a>
                 </p>
             </>
         );
