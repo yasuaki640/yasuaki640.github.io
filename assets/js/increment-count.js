eb7a7f74da4984ec7b35a04c4400d9cdd5e40c34
yasuaki640 <yasuaki640@gmail.com>
2024-04-19 23:20:13 +0900

    Fix JSON destructuring assignment in increment-count.js

--- assets/js/increment-count.js
+++ assets/js/increment-count.js
@@ -2,8 +2,7 @@
   const res = await fetch(
     "https://page-view-counter-api.yasuaki640.workers.dev/increment-count",
   );
-  const json = await res.json();
-  const count = json.count;
+  const { count } = await res.json();
 
   document.getElementById(
     "page-view-counter",
